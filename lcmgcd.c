9b97e16bfc604ad4a82d27f910a2b5e8592517fb
EternalLilies <3287857412@qq.com>
2024-09-24 08:20:45 +0800

    forth

---
+++ lcmgcd.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int a,b,c,p,m,n;
+	printf("请输入两个整数：\n");
+	scanf("%d %d",&a,&b);
+	m=a,n=b;
+	if (a<b)
+		c=b,b=a,a=c;
+	p=a%b;
+	while(p!=0)
+		{
+		a=b,b=p;
+		p=a%b;
+		}
+	c=m*n/b;
+	printf("%d和%d的最大公约数是%d\n",m,n,b);
+	printf("%d和%d的最小公倍数是%d\n",m,n,c);
+	return 0;
+}
