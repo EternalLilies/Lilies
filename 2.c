e0be9d497cd622596f43dfbd3761b90d44fc325d
EternalLilies <3287857412@qq.com>
2024-09-25 11:21:27 +0800

    nineth

--- 2.c
+++ 2.c
@@ -20,7 +20,7 @@ int main(){
 	int a,b,m,n;
 	scanf("%d %d %d %d",&n,&m,&a,&b);
 	unsigned long long p=combination(n+m,n);
-	unsigned long long q,p1,p2,p3,p4,p5,p6,p7,p8;
+	unsigned long long q,p1,p2,p3,p4,p5,p6,p7,p8,p9,p0;
 	if((a<3&&b<3)||(a>n-3&&b>m-3)){
 		q=0;
 	}
@@ -33,7 +33,9 @@ int main(){
 		p6=combination((a+b-2),(a+1))*combination((n+m-a-b+1),(n-a-1));
 		p7=combination((a+b-1),(a-3))*combination((n+m-a-b-1),(n-a+1));
 		p8=combination((a+b-1),(a+2))*combination((n+m-a-b-1),(n-a-2));
-		q=p-p1-p2-p3-p4-p5-p6-p7-p8;
+		p9=combination((a+b-3),(a-3))*combination((n+m-a-b-3),n-a);
+		p0=combination((a+b-3),a)*combination((n+m-a-b-3),(n-a-3));
+		q=p-p1-p2-p3-p4-p5-p6-p7-p8+p9+p0;
 	}
 	printf("%llu\n",q);
 	return 0;
