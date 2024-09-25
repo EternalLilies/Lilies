8a4233f5a48e529f6ea019d7f1fb05f8ac410444
EternalLilies <3287857412@qq.com>
2024-09-25 20:33:03 +0800

    nineth

--- 2.c
+++ 2.c
@@ -21,10 +21,23 @@ int main(){
 	scanf("%d %d %d %d",&n,&m,&a,&b);
 	unsigned long long p=combination(n+m,n);
 	unsigned long long q,p1,p2,p3,p4,p5,p6,p7,p8,p9,p0;
-	if((a<3&&b<3)||(a>n-3&&b>m-3)){
-		q=combination((a+b-3),(a-3))*combination((n+m-a-b-3),n-a)+combination((a+b-3),a)*combination((n+m-a-b-3),(n-a-3));
+	p1=combination((n+m-a-b-3),(n-a));
+	p2=combination((n+m-a-b-3),(n-a-3));
+	p3=combination((a+b-3),(a-3));
+	p4=combination((a+b-3),a);
+	if((a==2&&b==2)||(a==n-2&&b==m-2)||(a==0&&b==0)||(a==n&&b==m)||(a*b==2)||(a==n-1&&b==m-2)||(a==n-2&&b==m-1)){
+		q=0;
 	}
-	else{
+	if((a==1&&b==1)||(a==n-1&&b==m-1)){
+		q=p1+p2+p3+p4;
+	}
+	if((a==1&&b==0)||(a==2&&b==0)||(a==n-1&&b==m)||(a==n-2&&b==m)){
+		q=p1+p4;
+	}
+	if((a==0&&b==2)||(a==0&&b==1)||(a==n&&b==m-2)||(a==n&&b==m-1)){
+		q=p2+p3;
+	}
+	if((a>2||b>2)&&(a<n-2||b<m-2)){
 		p1=combination((a+b-3),(a-2))*combination((n+m-a-b+3),(n-a+2));
 		p2=combination((a+b-3),(a-1))*combination((n+m-a-b+3),(n-a+1));
 		p3=combination((a+b-3),(a-3))*combination((n+m-a-b+2),(n-a+2));
