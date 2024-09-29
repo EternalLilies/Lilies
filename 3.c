eb372934ab144989dfe5e5d54cc96e707ac20b39
EternalLilies <3287857412@qq.com>
2024-09-29 16:07:01 +0800

    tenth

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(){
+	int a1,a2,a3,a4,a5,a6,a7,a8,a9,a0,b,i;
+	scanf("%d %d %d %d %d %d %d %d %d %d\n%d",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a0,&b);
+	i=1;
+	if(a1<=b)i++;
+	if(a2<=b)i++;
+	if(a3<=b)i++;
+	if(a4<=b)i++;
+	if(a5<=b)i++;
+	if(a6<=b)i++;
+	if(a7<=b)i++;
+	if(a8<=b)i++;
+	if(a8<=b)i++;
+	if(a9<=b)i++;
+	if(a0<=b)i++;
+	return 0;
+}
