92098b601641ca0a2944d8dc947d45d9053717e7
EternalLilies <3287857412@qq.com>
2024-10-04 14:47:33 +0800

    eleventh

--- 6.c
+++ 6.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(){
-	unsigned n,k,i;
+	unsigned n,k,i,h;
 	scanf("%u %u",&n,&k);
 	i=0;
+	h=n;
 	for(;n/k!=0;){
 		i=i+n/k;
 		n=n/k+n%k;
 	}
-	printf("%u",i);
+	printf("%u",h+i);
 	return 0;
 }
