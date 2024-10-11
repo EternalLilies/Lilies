ba1c38a084e6f554d802aaeb1aa0da7d1d457333
EternalLilies <3287857412@qq.com>
2024-10-11 23:01:47 +0800

    fourteenth

---
+++ 9.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+	int i,a,b;
+	int j[12];
+	for(i=0;i<12;i++){
+		scanf("%d",&j[i]);
+	}
+	a=0;
+	for(i=0;;i++){
+		a=a+300;
+		a=a-j[i];
+		if(a<0){
+			printf("-%d",i+1);
+			break;
+		}
+		b=a/100;
+		a=a%100;
+		if(i==11){
+			printf("%d",a+100*b);
+			break;
+		}
+	}
+	return 0;
+}
